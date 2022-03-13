221a4267aa43f50570608e53b60432f200366c90
Hanafubuki <micheleweberr@gmail.com>
2022-03-13 23:04:55 +0900

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 # entrypoint.sh
 #!/bin/bash
-composer install 
 
-#!/usr/bin/env sh
-cp .env.example .env &
-composer require laravel/passport &
-php artisan migrate &
-php artisan passport:install &
-php artisan key:generate &
-php artisan optimize &
+composer install 
+cp .env.example .env
+composer require laravel/passport
+php artisan migrate
+php artisan passport:install
+php artisan key:generate
+php artisan optimize
 php artisan serve --host=0.0.0.0
